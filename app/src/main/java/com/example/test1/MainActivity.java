0b39644b0b8184c530157ef2f1766fbb88bf7d8d
Yaosobik <magomedovdinislam05@gmail.com>
2025-10-15 01:33:17 +0300

    0.0.3 realise calc

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.test1;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -28,5 +29,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resulText = findViewById(R.id.resulText1);
+        editTextText_1 = findViewById(R.id.editTextText_1);
+        editTextText_2 = findViewById(R.id.editTextText_2);
+        button_1 = findViewById(R.id.button_1);
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float num1 = Float.parseFloat(editTextText_1.getText().toString());
+                float num2 = Float.parseFloat(editTextText_2.getText().toString());
+                float res = num1 + num2;
+                resulText.setText(String.valueOf(res));
+            }
+        });
+
     }
 }
